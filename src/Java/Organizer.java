6c76204e2be908700f85d3d09b8f615d55b75a49
Patrick <p.leonhardt@hotmail.de>
2019-03-19 13:31:16 +0100

    Debug Class hinzugefügt

--- src/Java/Organizer.java
+++ src/Java/Organizer.java
@@ -1,21 +1,8 @@
 package Java;
 
-import Java.CreaterClasses.CreateDienst;
-import Java.CreaterClasses.CreateMitarbeiter;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Organizer {
     public static void main(String[] args){
-        List mList = new ArrayList();
-        Mitarbeiter m = new Mitarbeiter("Patrick", "Leonhardt", LocalDate.of(1995, 4, 14));
-        mList.add(m);
-        Dienst d = new Dienst(mList, LocalDateTime.of(2019, 3, 10, 14, 0), LocalDateTime.of(2019, 3, 10, 18, 0));
-        CreateDienst createD = new CreateDienst();
-
-        CreateMitarbeiter createMitarbeiter = new CreateMitarbeiter();
+       DebugClass debugger = new DebugClass();
     }
 }
