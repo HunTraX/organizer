ee7a7c7892f41a2898b58391503d448c572bc017
Patrick <p.leonhardt@hotmail.de>
2019-04-26 21:33:59 +0200

    New IO Classes for Diensttemplate - Copy Constructor - CreateMitarbeiter mit TryCatch versewhen

--- src/Java/CreaterClasses/CreateMitarbeiter.java
+++ src/Java/CreaterClasses/CreateMitarbeiter.java
@@ -16,26 +16,31 @@ public class CreateMitarbeiter {
     private static String geburtsDatumAsString;
     private static double stdBeschränkung=0;
     private static LocalDate geburtsDatum;
-    
+
     private CreateMitarbeiter(ArrayList<Mitarbeiter> mList){
 
     }
 
     public static void createMitarbeiter(ArrayList<Mitarbeiter> liste){
-        sc = new Scanner(System.in);
-        System.out.println("Bitte geben sie ihren Nachnamen ein:");
-        name = sc.nextLine();
-        System.out.println("Bitte geben sie ihren Voramen ein:");
-        vorName = sc.nextLine();
-        // Geburtsdatum muss nach YYYY-MM-DD angegeben werden.
-        System.out.println("Bitte geben sie ihr Geburtsdatum ein: (YYYY-MM-DD)");
-        geburtsDatumAsString = sc.next();
-        System.out.println(geburtsDatumAsString);
+        try{
+            sc = new Scanner(System.in);
+            System.out.println("Bitte geben sie ihren Nachnamen ein:");
+            name = sc.nextLine();
+            System.out.println("Bitte geben sie ihren Voramen ein:");
+            vorName = sc.nextLine();
+            // Geburtsdatum muss nach YYYY-MM-DD angegeben werden.
+            System.out.println("Bitte geben sie ihr Geburtsdatum ein: (YYYY-MM-DD)");
+            geburtsDatumAsString = sc.next();
+            System.out.println(geburtsDatumAsString);
 
-        geburtsDatum = LocalDate.parse(geburtsDatumAsString);
+            geburtsDatum = LocalDate.parse(geburtsDatumAsString);
 
-        Mitarbeiter m1 = new Mitarbeiter(name, vorName, geburtsDatum);
-        liste.add(m1);
+            Mitarbeiter m1 = new Mitarbeiter(name, vorName, geburtsDatum);
+            liste.add(m1);
+        } catch (Exception e){
+            System.out.println("Fehler: " + e.getMessage());
+        }
+
     }
 
 
--- src/Java/DebugClass.java
+++ src/Java/DebugClass.java
@@ -1,18 +1,29 @@
 package Java;
 
 import Java.CreaterClasses.CreateMitarbeiter;
+import Java.CreaterClasses.CreateTemplate;
 import Java.IOClasses.LoadMitarbeiter;
+import Java.IOClasses.LoadTemplate;
 import Java.IOClasses.SaveMitarbeiter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DebugClass {
     public DebugClass() {
 
+        Scanner sc = new Scanner(System.in);
 
+        ArrayList<DienstTemplate> dienstTemplateArrayList = LoadTemplate.load();
         ArrayList<Mitarbeiter> mList = LoadMitarbeiter.load();
-        CreateMitarbeiter.createMitarbeiter(mList);
+        while(true){
+            CreateMitarbeiter.createMitarbeiter(mList);
+            if (sc.next().equals("X")){
+                break;
+            }
+        }
+        CreateTemplate.createTemplate(dienstTemplateArrayList);
         for(Mitarbeiter mitarbeiter: mList){
             mitarbeiter.debugMitarbeiter();
         }
